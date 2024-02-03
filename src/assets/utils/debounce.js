cb7b2cc7cbfc956c4c18afb6ba4b288b1dbbb644
Kenny Lozeau <kenny.lozeau@gmail.com>
2024-02-03 16:37:31 -0500

    Add debounce util function

---
+++ src/assets/utils/debounce.js
@@ -0,0 +1,14 @@
+function debounce(fn, interval) {
+    let timeout;
+  
+    return (...args) => {
+      const fnCall = () => {
+        timeout = null;
+        fn(...args)
+      }
+      clearTimeout(timeout);
+      timeout = setTimeout(fnCall, interval);
+    }
+  }
+  
+  export default debounce;
